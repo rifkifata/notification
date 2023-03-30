7e23f0496a8e374720f4d641790188b01b1c3343
Teuku Rifki Dhulul Fata <u073590@bca.co.id>
2023-03-30 14:45:57 +0700

    Initial commit

---
+++ app/src/main/java/com/iki/notification/MainActivity.java
@@ -0,0 +1,26 @@
+package com.iki.notification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            InstanceID instanceID = InstanceID.getInstance(this);
+
+            String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),
+                    GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+
+            Log.i(TAG, "GCM Registration Token: " + token);
+
+        }catch (Exception e) {
+            Log.d(TAG, "Failed to complete token refresh", e);
+        }
+    }
+}
